d9d520e56184c2aa9015616234dab1a756c592cc
ottenokLeshi <ottenok.belogo@gmail.com>
2017-03-13 15:04:18 +0300

    Added fuction, that gives file to client

--- public/script.js
+++ public/script.js
@@ -1,5 +1,14 @@
 const getData = () => {
     fetch('/routes', {
         body: Blob
-    }).then(response => console.log(response.body));
+    }).then(response => response.blob())
+    .then(blob => {
+        const link = document.createElement('a');
+        document.body.appendChild(link);
+        link.style = 'display: none';
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'MyFile';
+        link.click();
+        document.removeChild(link);
+    });
 };
